a39d304cd6147dae018d3bcd585bde72ab89f3c8
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-05-16 15:28:39 +0530

    initial next js

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,7 @@
+
+
+function AboutUsPage() {
+    return <h1>The About US Page</h1>
+  }
+  
+  export default AboutUsPage;
---
+++ src/app/news/page.tsx
@@ -0,0 +1,7 @@
+
+
+function NewsPage() {
+    return <h1>The News Page</h1>
+  }
+  
+  export default NewsPage;
