a751c132cf8bc228236a04d2114a38b4e89e76a2
sumitbhardwaj07 <150799672+sumitbhardwaj07@users.noreply.github.com>
2024-05-16 17:35:41 +0530

    some modifications

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
 
 
 function AboutUsPage() {
-    return <h1>The About US Page</h1>
+
+  const details = [
+    { id: '1', name: 'Yash', role: 'Senior Developer' },
+    { id: '2', name: 'Vaibhav', role: 'Backend Developer' },
+    { id: '3', name: 'Suresh', role: 'Frontend Developer' }
+  ];
+
+    return (
+      <div>
+      <h1>Our Developers</h1>
+      <ul>
+        {details.map(detail => (
+          <li key={detail.id}>
+            <Link href={`/aboutus/${detail.id}`}>
+              <h3>{detail.name}</h3>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+    )
   }
   
   export default AboutUsPage;
